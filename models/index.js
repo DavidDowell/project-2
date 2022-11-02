b6428202bfa0dce27ddc89a2eb4a0d7f9f34e736
JGibbings <j_gibbings@msn.com>
2022-11-02 19:58:43 -0400

    update index for models

--- models/index.js
+++ models/index.js
@@ -1,11 +1,8 @@
-const { User, Post } = require("./User");
+const { User, Post, Vote } = require("./User");
 
-module.exports = {
-  User,
-  Post,
-};
 const Post = require("./Post");
 const User = require("./User");
+const Vote = require("./Vote");
 
 User.hasMany(Post, {
   foreignKey: "user_id",
@@ -14,7 +11,33 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
+User.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
+});
+Post.belongsToMany(User, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id",
+});
+
+User.hasMany(Vote, {
+  foreignKey: "user_id",
+});
+Vote.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Vote, {
+  foreignKey: "post_id",
+});
+Vote.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
 module.exports = {
   Post,
   User,
+  Vote,
 };
