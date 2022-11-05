04f065baa48192914f48b72da7a8b67a2ae7629b
JGibbings <j_gibbings@msn.com>
2022-11-04 21:01:53 -0400

    add api route files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,22 @@
-const router = require('express').Router();
-const { UserController } = require('../../controllers');
+const router = require("express").Router();
+const { UserController } = require("../../controllers");
+const userRoutes = require("./user-routes");
+const postRoutes = require("./post-routes");
+const commentRoutes = require("./comment-routes");
+const voteRoutes = require("./vote-routes");
+const tagRoutes = require("./tag-routes");
+const postTagRoutes = require("./postTag-routes");
 
-const isAuthenticated = require('../../middleware/isAuthenticated');
+const isAuthenticated = require("../../middleware/isAuthenticated");
 
-router.post('/register', UserController.register);
-router.post('/login', UserController.login);
-router.post('/logout', isAuthenticated, UserController.logout);
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
+router.post("/logout", isAuthenticated, UserController.logout);
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
+router.use("/votes", voteRoutes);
+router.use("/tags", tagRoutes);
+router.use("/postTags", postTagRoutes);
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
+
+// GET all /api/users
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+// GET one /api/users/:id
+// POST /api/users
+// PUT /api/users/:id
+// DELETE /api/users/:id
