0d30c2bbc250a6f8b82ebb7161bb195c4ca164ac
JGibbings <j_gibbings@msn.com>
2022-11-06 20:19:07 -0500

    commit for pull

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,6 @@ const { UserController } = require("../../controllers");
 const userRoutes = require("./user-routes");
 const postRoutes = require("./post-routes");
 const commentRoutes = require("./comment-routes");
-const voteRoutes = require("./vote-routes");
 const tagRoutes = require("./tag-routes");
 const postTagRoutes = require("./postTag-routes");
 
@@ -15,7 +14,6 @@ router.post("/logout", isAuthenticated, UserController.logout);
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
 router.use("/comments", commentRoutes);
-// router.use("/votes", voteRoutes);
 // router.use("/tags", tagRoutes);
 // router.use("/postTags", postTagRoutes);
 
