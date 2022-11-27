d8cd6d242e4e831fc095810c3609d72aab27f399
Joshua Bohde <bohdecoded@gmail.com>
2022-11-27 15:23:07 -0500

    refactor: extract comment functionality to CommentController

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const PageController = require('./PageController');
 const UserController = require('./UserController');
+const CommentController = require('./CommentController');
 
 module.exports = {
   PageController,
   UserController,
-}
+  CommentController
+};
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,45 +1,8 @@
 const router = require("express").Router();
-const { Comment } = require("../../models");
+const { CommentController } = require("../../controllers");
 
-router.get("/", (req, res) => {
-  Comment.findAll()
-    .then((dbCommentData) => res.json(dbCommentData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.post("/", (req, res) => {
-  Comment.create({
-    comment_text: req.body.comment_text,
-    user_id: req.session.user_id,
-    post_id: req.body.post_id,
-  })
-    .then((dbCommentData) => res.json(dbCommentData))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-});
-
-router.delete("/:id", (req, res) => {
-  Comment.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((dbCommentData) => {
-      if (!dbCommentData) {
-        res.status(404).json({ message: "No comment found with this id!" });
-        return;
-      }
-      res.json(dbCommentData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+router.get("/", CommentController.getAllComments);
+router.post("/", CommentController.createComment);
+router.delete("/:id", CommentController.deleteComment);
 
 module.exports = router;
