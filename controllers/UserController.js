0dd1960306be6f2f6a86f488eed1e9509a9bb87d
Joshua Bohde <bohdecoded@gmail.com>
2022-11-27 16:05:40 -0500

    refactor: extract user api functionality to UserController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,4 @@
-const router = require("express").Router();
-const { User } = require("../models");
+const { User, Post, Comment, Vote } = require("../models");
 
 // CREATE new user
 module.exports = {
@@ -80,4 +79,116 @@ module.exports = {
       res.status(404).end();
     }
   },
+
+  createUser: (req, res) => {
+    User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+
+          res.json(dbUserData);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  getAllUsers: (req, res) => {
+    User.findAll({
+      attributes: { exclude: ["password"] },
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  getSingleUser: (req, res) => {
+    User.findOne({
+      attributes: { exclude: ["password"] },
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ["id", "title", "post_text", "created_at"],
+        },
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "created_at"],
+          include: {
+            model: Post,
+            attributes: ["title"],
+          },
+        },
+        {
+          model: Post,
+          attributes: ["title"],
+          through: Vote,
+          as: "voted_posts",
+        },
+      ],
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  updateUser: (req, res) => {
+    User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  deleteUser: (req, res) => {
+    User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 };
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,121 +1,15 @@
 const router = require("express").Router();
-const { User, Post, Comment, Vote } = require("../../models");
+const { UserController } = require("../../controllers");
 
 // GET all /api/users
-router.get("/", (req, res) => {
-  User.findAll({
-    attributes: { exclude: ["password"] },
-  })
-    .then((dbUserData) => res.json(dbUserData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
+router.get("/", UserController.getAllUsers);
 // GET one /api/users/:id
-router.get("/:id", (req, res) => {
-  User.findOne({
-    attributes: { exclude: ["password"] },
-    where: {
-      id: req.params.id,
-    },
-    include: [
-      {
-        model: Post,
-        attributes: ["id", "title", "post_text", "created_at"],
-      },
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "created_at"],
-        include: {
-          model: Post,
-          attributes: ["title"],
-        },
-      },
-      {
-        model: Post,
-        attributes: ["title"],
-        through: Vote,
-        as: "voted_posts",
-      },
-    ],
-  })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this id" });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
+router.get("/:id", UserController.getSingleUser);
 // POST /api/users
-router.post("/", (req, res) => {
-  User.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-  })
-    .then((dbUserData) => {
-      req.session.save(() => {
-        req.session.user_id = dbUserData.id;
-        req.session.username = dbUserData.username;
-        req.session.loggedIn = true;
-
-        res.json(dbUserData);
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
+router.post("/", UserController.createUser);
 // PUT /api/users/:id
-router.put("/:id", (req, res) => {
-  User.update(req.body, {
-    individualHooks: true,
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this id" });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
+router.put("/:id", UserController.updateUser);
 // DELETE /api/users/:id
-router.delete("/:id", (req, res) => {
-  User.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this id" });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+router.delete("/:id", UserController.deleteUser);
 
 module.exports = router;
